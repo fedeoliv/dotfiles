44275e45335d4c2e4f409da2f1433bb788d886ed
Fernando de Oliveira <5161098+fedeoliv@users.noreply.github.com>
2023-02-23 09:05:00 -0500

    feature: markdown-preview plugin added

--- nvim/lua/plugins/utilities.lua
+++ nvim/lua/plugins/utilities.lua
@@ -1,4 +1,13 @@
 return {
   "christoomey/vim-tmux-navigator", -- Seamless navigation between tmux panes and vim splits
   "mfussenegger/nvim-jdtls", -- LSP extensions for eclipse jdtls
+
+  -- Markdown preview
+  {
+    "iamcco/markdown-preview.nvim",
+    -- build = "cd app && npm install",
+    config = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
 }
