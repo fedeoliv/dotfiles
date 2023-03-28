f68ab4a7b0f3383bd96fb620a064c2dc20bd3e15
Fernando de Oliveira <5161098+fedeoliv@users.noreply.github.com>
2023-03-28 07:59:34 -0400

    feature: neovide config updated

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -15,10 +15,11 @@ if vim.g.neovide then
   local alpha = function()
     return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
   end
-  -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
-  -- vim.g.neovide_transparency = 0.0
-  -- vim.g.transparency = 0.8
-  -- vim.g.neovide_background_color = "#0f1117" .. alpha()
+
+  -- transparency
+  vim.g.neovide_transparency = 0.0
+  vim.g.transparency = 0.98
+  vim.g.neovide_background_color = "#0f1117" .. alpha()
 
   -- blur
   vim.g.neovide_floating_blur_amount_x = 2.0
