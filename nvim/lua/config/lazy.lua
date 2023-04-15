0a46ff959f9ab3221e4b0f514f12460d648b8704
Fernando de Oliveira <5161098+fedeoliv@users.noreply.github.com>
2023-04-14 21:37:05 -0400

    feature: eslint and prettier setup

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -10,10 +10,16 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+
+    -- eslint for fix on save and prettier for formatting
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+
     -- import/override with your plugins
     { import = "plugins" },
   },
