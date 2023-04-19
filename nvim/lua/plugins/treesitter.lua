27b2eef88c8a6ec3d5d3000661dbdbd60fe02056
Fernando de Oliveira <5161098+fedeoliv@users.noreply.github.com>
2023-04-19 08:16:03 -0400

    feature: treesitter ensure_installed list updated

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,37 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  event = "VeryLazy",
+  event = { "BufReadPost", "BufNewFile" },
   opts = function(_, opts)
+    if type(opts.ensure_installed) == "table" then
+      vim.list_extend(opts.ensure_installed, {
+        "bash",
+        "c_sharp",
+        "dockerfile",
+        "go",
+        "hcl",
+        "help",
+        "html",
+        "http",
+        "java",
+        "javascript",
+        "json",
+        "json5",
+        "jsonc",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "mermaid",
+        "python",
+        "query",
+        "regex",
+        "sql",
+        "terraform",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      })
+    end
     opts.ensure_installed = {
       "bash",
       "c_sharp",
