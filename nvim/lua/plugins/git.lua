36a0655ede149c78b6d047413656b52a93cc3009
Fernando de Oliveira <5161098+fedeoliv@users.noreply.github.com>
2023-04-22 15:23:52 -0400

    feature: gitsigns and diffview configs added

---
+++ nvim/lua/plugins/git.lua
@@ -0,0 +1,51 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      signs = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "▎" },
+        untracked = { text = "▎" },
+      },
+      current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        delay = 1000,
+        ignore_whitespace = false,
+      },
+      current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+      signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+      numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+      linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+      word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+      watch_gitdir = { interval = 1000, follow_files = true },
+      attach_to_untracked = true,
+      sign_priority = 6,
+      update_debounce = 100,
+      status_formatter = nil, -- Use default
+      max_file_length = 40000,
+      preview_config = {
+        -- Options passed to nvim_open_win
+        border = "rounded",
+        style = "minimal",
+        relative = "cursor",
+        row = 0,
+        col = 1,
+      },
+      yadm = { enable = false },
+    },
+
+    -- tabpage for checking modified files
+    {
+      "sindrets/diffview.nvim",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+      },
+      event = "VeryLazy",
+    },
+  },
+}
