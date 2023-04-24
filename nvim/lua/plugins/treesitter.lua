b792175f4863a670fcf1ed8b3c2850101ca70c2c
Fernando de Oliveira <5161098+fedeoliv@users.noreply.github.com>
2023-04-24 11:46:26 -0400

    feature: nvim-ts-rainbow2 added

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,39 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPost", "BufNewFile" },
-  opts = function(_, opts)
-    if type(opts.ensure_installed) == "table" then
-      vim.list_extend(opts.ensure_installed, {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = function(_, opts)
+      -- if type(opts.ensure_installed) == "table" then
+      --   vim.list_extend(opts.ensure_installed, {
+      --     "bash",
+      --     "c_sharp",
+      --     "dockerfile",
+      --     "go",
+      --     "hcl",
+      --     "help",
+      --     "html",
+      --     "http",
+      --     "java",
+      --     "javascript",
+      --     "json",
+      --     "json5",
+      --     "jsonc",
+      --     "lua",
+      --     "markdown",
+      --     "markdown_inline",
+      --     "mermaid",
+      --     "python",
+      --     "query",
+      --     "regex",
+      --     "sql",
+      --     "terraform",
+      --     "tsx",
+      --     "typescript",
+      --     "vim",
+      --     "yaml",
+      --   })
+      -- end
+      opts.ensure_installed = {
         "bash",
         "c_sharp",
         "dockerfile",
@@ -15,8 +45,6 @@ return {
         "java",
         "javascript",
         "json",
-        "json5",
-        "jsonc",
         "lua",
         "markdown",
         "markdown_inline",
@@ -30,33 +58,23 @@ return {
         "typescript",
         "vim",
         "yaml",
-      })
-    end
-    opts.ensure_installed = {
-      "bash",
-      "c_sharp",
-      "dockerfile",
-      "go",
-      "hcl",
-      "help",
-      "html",
-      "http",
-      "java",
-      "javascript",
-      "json",
-      "lua",
-      "markdown",
-      "markdown_inline",
-      "mermaid",
-      "python",
-      "query",
-      "regex",
-      "sql",
-      "terraform",
-      "tsx",
-      "typescript",
-      "vim",
-      "yaml",
-    }
-  end,
+      }
+
+      opts.rainbow = {
+        enable = true,
+        -- disable = { "jsx", "cpp" },
+        -- Which query to use for finding delimiters
+        query = "rainbow-parens",
+        -- Highlight the entire buffer all at once
+        strategy = require("ts-rainbow").strategy.global,
+      }
+    end,
+  },
+  {
+    "HiPhish/nvim-ts-rainbow2",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    event = "VeryLazy",
+  },
 }
