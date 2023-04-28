616ecb5c9f7ad2dfb0e33a4ef6e1944f5d084436
Fernando de Oliveira <5161098+fedeoliv@users.noreply.github.com>
2023-04-27 20:31:52 -0400

    refactor: text-related plugins moved to text.lua

---
+++ nvim/lua/plugins/text.lua
@@ -0,0 +1,19 @@
+return {
+  -- move lines and selections up and down
+  {
+    "matze/vim-move",
+    event = "VeryLazy",
+  },
+
+  -- replace existing text with contents from a register
+  {
+    "vim-scripts/ReplaceWithRegister",
+    event = "VeryLazy",
+  },
+
+  -- w,e,b motions considering camelCase, snake_case and kebab-case as punctuation
+  {
+    "chrisgrieser/nvim-spider",
+    event = "VeryLazy",
+  },
+}
--- nvim/lua/plugins/utilities.lua
+++ nvim/lua/plugins/utilities.lua
@@ -29,22 +29,4 @@ return {
       vim.fn["mkdp#util#install"]()
     end,
   },
-
-  -- move lines and selections up and down
-  {
-    "matze/vim-move",
-    event = "VeryLazy",
-  },
-
-  -- replace existing text with contents from a register
-  {
-    "vim-scripts/ReplaceWithRegister",
-    event = "VeryLazy",
-  },
-
-  -- w,e,b motions considering camelCase, snake_case and kebab-case as punctuation
-  {
-    "chrisgrieser/nvim-spider",
-    event = "VeryLazy",
-  },
 }
