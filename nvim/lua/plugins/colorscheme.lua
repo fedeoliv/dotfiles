13a06dfeef2db39e94afa4d57ed1af0d41c97e84
Fernando de Oliveira <5161098+fedeoliv@users.noreply.github.com>
2023-12-15 22:05:35 -0500

    chore: colorscheme config cleanup

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,40 +1,22 @@
 return {
-  -- {
-  --   "folke/tokyonight.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   opts = {
-  --     transparent = true,
-  --   },
-  --   -- enabled = false,
-  -- },
-  -- {
-  --   "catppuccin/nvim",
-  --   name = "catppuccin",
-  --   priority = 1000,
-  -- },
   {
-    "bluz71/vim-nightfly-colors",
-    name = "nightfly",
-    lazy = true,
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
+    opts = {
+      transparent_background = true,
+    },
   },
   -- {
-  --   "kvrohit/mellow.nvim",
+  --   "bluz71/vim-nightfly-colors",
+  --   name = "nightfly",
   --   lazy = true,
   --   priority = 1000,
-  --   enabled = false,
-  -- },
-  -- {
-  --   "marko-cerovac/material.nvim",
-  --   lazy = true,
-  --   priority = 1000,
-  --   enabled = false,
   -- },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nightfly",
+      colorscheme = "catppuccin-mocha",
     },
   },
 }
