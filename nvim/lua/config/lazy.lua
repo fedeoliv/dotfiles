ad0194f798076862a81b3cbd1b0465c8f01cc83c
Fernando de Oliveira <5161098+fedeoliv@users.noreply.github.com>
2023-12-22 08:56:13 -0500

    feature: lazy.nvim ui config and plugin updated notification off

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -13,6 +13,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "plugins" },
   },
+
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
     -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
@@ -22,8 +23,17 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  -- install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+
+  ui = {
+    border = "double",
+    size = {
+      width = 0.8,
+      height = 0.8,
+    },
+  },
+
+  checker = { enabled = true, notify = false }, -- automatically check for plugin updates
+
   performance = {
     rtp = {
       -- disable some rtp plugins
