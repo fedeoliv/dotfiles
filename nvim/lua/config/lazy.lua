2a502dd3d13c3ea365803ed94bb6e607e40d3b6d
Fernando de Oliveira <5161098+fedeoliv@users.noreply.github.com>
2024-01-02 14:14:07 -0500

    feature: lazyvim python and dap plugins added

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -11,6 +11,8 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.dap.core" },
     { import = "plugins" },
   },
 
