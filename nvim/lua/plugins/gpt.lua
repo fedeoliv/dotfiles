a131b9bbdd1e0232aa589b6524a92d13e4009605
Fernando de Oliveira <5161098+fedeoliv@users.noreply.github.com>
2024-02-03 20:07:16 -0500

    feature: chatgpt.nvim plugin added

---
+++ nvim/lua/plugins/gpt.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup({
+        api_key_cmd = "dcli p azure-openai --output password",
+      })
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "folke/trouble.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+  },
+}
