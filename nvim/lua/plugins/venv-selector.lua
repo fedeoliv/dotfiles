7f1c76d25331901341f4956497d15bfc4f2f583c
Fernando de Oliveira <5161098+fedeoliv@users.noreply.github.com>
2024-02-24 20:44:14 -0500

    feature: venv-selector.nvim plugin added

---
+++ nvim/lua/plugins/venv-selector.lua
@@ -0,0 +1,9 @@
+return {
+  "linux-cultist/venv-selector.nvim",
+  dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+  event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+  keys = {
+    { "<leader>vs", "<cmd>VenvSelect<cr>", desc = "Select Python venv" },
+    { "<leader>vc", "<cmd>VenvSelectCached<cr>", desc = "Retrieve Python venv from cache" },
+  },
+}
