fce6c0fa2e96946633415de2ca4b062e02f66691
Fernando de Oliveira <fedeoliv@Fernandos-MBP.attlocal.net>
2024-02-27 19:55:31 -0500

    feature: csharp.nvim plugin added

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -12,3 +12,23 @@ vim.api.nvim_create_autocmd({ "ColorScheme" }, {
     vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "NONE", ctermbg = "NONE" })
   end,
 })
+
+-- automatically format C# code on save
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = args.buf,
+      callback = function()
+        -- Format the code before you run fix usings
+        vim.lsp.buf.format({ timeout = 1000, async = false })
+
+        -- If the file is C# then run fix usings
+        if vim.bo[0].filetype == "cs" then
+          require("csharp").fix_usings()
+        end
+      end,
+    })
+  end,
+})
