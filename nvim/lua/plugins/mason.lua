a901b40cc1598f4fdf32ab6d0f21ca9cc69ad951
Fernando de Oliveira <fedeoliv@Fernandos-MBP.attlocal.net>
2024-03-18 20:59:17 -0400

    feature: basedpyright lsp setup

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -108,20 +108,22 @@ return {
   },
   opts = {
     servers = {
-      pyright = {
-        settings = {
-          pyright = {
-            -- Using Ruff's import organizer
-            disableOrganizeImports = true,
-          },
-          python = {
-            analysis = {
-              -- Ignore all files for analysis to exclusively use Ruff for linting
-              ignore = { "*" },
-            },
-          },
-        },
-      },
+      basedpyright = {},
+      -- pyright = {
+      --   cmd = { "basedpyright-langserver", "--stdio" },
+      --   settings = {
+      --     pyright = {
+      --       -- Using Ruff's import organizer
+      --       disableOrganizeImports = true,
+      --     },
+      --     python = {
+      --       analysis = {
+      --         -- Ignore all files for analysis to exclusively use Ruff for linting
+      --         ignore = { "*" },
+      --       },
+      --     },
+      --   },
+      -- },
       ruff_lsp = {},
     },
     setup = {
