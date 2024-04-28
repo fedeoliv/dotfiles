50e9335050291e4939dffd0dcb2f0587aeefa87e
Fernando de Oliveira <5161098+fedeoliv@users.noreply.github.com>
2024-04-28 12:55:02 -0400

    chore: unused colorscheme config removed

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -23,12 +23,6 @@ return {
       },
     },
   },
-  -- {
-  --   "bluz71/vim-nightfly-colors",
-  --   name = "nightfly",
-  --   lazy = true,
-  --   priority = 1000,
-  -- },
   {
     "LazyVim/LazyVim",
     opts = {
