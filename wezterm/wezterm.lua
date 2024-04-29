40a61d36f5408dc048cad68cabe58f2412bae26a
Fernando de Oliveira <5161098+fedeoliv@users.noreply.github.com>
2024-04-28 20:54:20 -0400

    feature: window opacity and blur

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,13 +7,15 @@ if wezterm.config_builder then
 end
 
 config.color_scheme = "Catppuccin Mocha"
+
 config.window_decorations = "RESIZE"
--- config.window_background_opacity = 0.90
--- config.macos_window_background_blur = 30
+config.window_background_opacity = 0.80
+config.macos_window_background_blur = 30
 
 -- config.font = wezterm.font("CaskaydiaCove Nerd Font", { weight = "Regular", italic = false })
 -- config.font = wezterm.font("MonoLisaMedium Nerd Font", { weight = "Regular", italic = false })
 config.font = wezterm.font("JetBrains Mono", { weight = "Medium", italic = false })
+
 config.font_size = 13.0
 
 config.enable_tab_bar = false
