e81020b9347edd9e8218c36f53a8f779dabb6d96
Fernando de Oliveira <5161098+fedeoliv@users.noreply.github.com>
2024-06-13 14:12:02 -0400

    feature: precognition setup and keymap

--- nvim/lua/plugins/utilities.lua
+++ nvim/lua/plugins/utilities.lua
@@ -46,7 +46,41 @@ return {
       })
     end,
   },
+
   {
     "tris203/precognition.nvim",
+    event = "VeryLazy",
+    config = {
+      startVisible = false,
+      showBlankVirtLine = true,
+      highlightColor = { link = "Comment" },
+      hints = {
+        Caret = { text = "^", prio = 2 },
+        Dollar = { text = "$", prio = 1 },
+        MatchingPair = { text = "%", prio = 5 },
+        Zero = { text = "0", prio = 0 },
+        w = { text = "w", prio = 10 },
+        b = { text = "b", prio = 9 },
+        e = { text = "e", prio = 8 },
+        W = { text = "W", prio = 7 },
+        B = { text = "B", prio = 6 },
+        E = { text = "E", prio = 5 },
+      },
+      gutterHints = {
+        G = { text = "G", prio = 10 },
+        gg = { text = "gg", prio = 9 },
+        PrevParagraph = { text = "{", prio = 8 },
+        NextParagraph = { text = "}", prio = 8 },
+      },
+    },
+    keys = {
+      {
+        "<leader>pt",
+        desc = "Toggle precognition",
+        function()
+          require("precognition").toggle()
+        end,
+      },
+    },
   },
 }
