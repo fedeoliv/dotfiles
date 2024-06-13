d76220cb03b3214d2afd457af05859672b1edf04
Fernando de Oliveira <5161098+fedeoliv@users.noreply.github.com>
2024-06-13 13:19:55 -0400

    feature: auto-session plugin added

--- nvim/lua/plugins/utilities.lua
+++ nvim/lua/plugins/utilities.lua
@@ -37,4 +37,16 @@ return {
       })
     end,
   },
+  {
+    "rmagatti/auto-session",
+    config = function()
+      require("auto-session").setup({
+        log_level = "error",
+        auto_session_suppress_dirs = { "~/", "~/projects", "~/Downloads", "/" },
+      })
+    end,
+  },
+  {
+    "tris203/precognition.nvim",
+  },
 }
