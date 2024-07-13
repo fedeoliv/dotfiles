27fe02e7b2ba1079694063d93fcb9050cf85894f
Fernando de Oliveira <5161098+fedeoliv@users.noreply.github.com>
2024-07-13 09:56:12 -0400

    feature: markview plugin

---
+++ nvim/lua/plugins/markview.lua
@@ -0,0 +1,9 @@
+return {
+  "OXY2DEV/markview.nvim",
+  ft = { "md" },
+  event = "VeryLazy",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+}
