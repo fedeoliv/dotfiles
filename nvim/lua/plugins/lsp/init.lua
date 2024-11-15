855ddc47c9fee941801e0ae391ddd17e93b8b171
Fernando de Oliveira <5161098+fedeoliv@users.noreply.github.com>
2024-11-15 15:13:38 -0500

    chore: transparent window background for fidget

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -131,7 +131,11 @@ return {
   {
     "j-hui/fidget.nvim",
     opts = {
-      -- options
+      notification = {
+        window = {
+          winblend = 0,
+        },
+      },
     },
   },
 }
