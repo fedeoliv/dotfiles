0cbaddf9cb0ba46b3f75443c6458f12e0095e3b1
Fernando de Oliveira <5161098+fedeoliv@users.noreply.github.com>
2025-03-19 08:59:06 -0400

    feat: gruvbox material colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -10,9 +10,18 @@ return {
     },
   },
   {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_enable_italic = true
+      vim.g.gruvbox_material_transparent_background = 2
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tundra",
+      colorscheme = "gruvbox-material",
     },
   },
 }
