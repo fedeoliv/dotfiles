87fa1d5806f2f30aad08d2a6e04973db0d6ca3e7
Fernando de Oliveira <5161098+fedeoliv@users.noreply.github.com>
2025-03-19 08:56:11 -0400

    feat: gruvbox material colorscheme and jetbrains mono font

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 local config = {}
 
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 config.automatically_reload_config = true
@@ -19,61 +19,62 @@ config.window_close_confirmation = "NeverPrompt"
 config.window_decorations = "RESIZE" -- disable title bar without disabling the resiable border
 config.macos_window_background_blur = 30
 config.window_padding = {
-  -- left = 3,
-  -- right = 3,
-  -- top = 25,
-  -- bottom = 0,
+	-- left = 3,
+	-- right = 3,
+	-- top = 25,
+	-- bottom = 0,
 }
 
-config.color_scheme = "tundra"
+-- config.color_scheme = "tundra"
+config.color_scheme = "Gruvbox dark, hard (base16)"
 
-config.font = wezterm.font("MonoLisaMedium Nerd Font", { weight = "Regular", italic = false })
+-- config.font = wezterm.font("MonoLisaMedium Nerd Font", { weight = "Regular", italic = false })
 -- config.font = wezterm.font("CaskaydiaCove Nerd Font", { weight = "Regular", italic = false })
--- config.font = wezterm.font("JetBrains Mono", { weight = "Medium", italic = false })
-config.font_size = 15.0
+config.font = wezterm.font("JetBrains Mono", { weight = "Medium", italic = false })
+config.font_size = 16.0
 
 -- URLs in Markdown files are not handled properly by default
 -- Source: https://github.com/wez/wezterm/issues/3803#issuecomment-1608954312
 config.hyperlink_rules = {
-  -- Matches: a URL in parens: (URL)
-  {
-    regex = "\\((\\w+://\\S+)\\)",
-    format = "$1",
-    highlight = 1,
-  },
-  -- Matches: a URL in brackets: [URL]
-  {
-    regex = "\\[(\\w+://\\S+)\\]",
-    format = "$1",
-    highlight = 1,
-  },
-  -- Matches: a URL in curly braces: {URL}
-  {
-    regex = "\\{(\\w+://\\S+)\\}",
-    format = "$1",
-    highlight = 1,
-  },
-  -- Matches: a URL in angle brackets: <URL>
-  {
-    regex = "<(\\w+://\\S+)>",
-    format = "$1",
-    highlight = 1,
-  },
-  -- Then handle URLs not wrapped in brackets
-  {
-    -- Before
-    --regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
-    --format = '$0',
-    -- After
-    regex = "[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)",
-    format = "$1",
-    highlight = 1,
-  },
-  -- implicit mailto link
-  {
-    regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
-    format = "mailto:$0",
-  },
+	-- Matches: a URL in parens: (URL)
+	{
+		regex = "\\((\\w+://\\S+)\\)",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Matches: a URL in brackets: [URL]
+	{
+		regex = "\\[(\\w+://\\S+)\\]",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Matches: a URL in curly braces: {URL}
+	{
+		regex = "\\{(\\w+://\\S+)\\}",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Matches: a URL in angle brackets: <URL>
+	{
+		regex = "<(\\w+://\\S+)>",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Then handle URLs not wrapped in brackets
+	{
+		-- Before
+		--regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
+		--format = '$0',
+		-- After
+		regex = "[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)",
+		format = "$1",
+		highlight = 1,
+	},
+	-- implicit mailto link
+	{
+		regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
+		format = "mailto:$0",
+	},
 }
 
 return config
