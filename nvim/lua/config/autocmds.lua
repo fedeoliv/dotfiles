e3429e092e5b34b6b7a5bc847ccc62e0d9400bc7
Fernando de Oliveira <5161098+fedeoliv@users.noreply.github.com>
2025-05-10 10:45:29 -0400

    feat: spell setup for pt-br

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,6 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
 -- Transparent backgrounds setup
 vim.api.nvim_create_augroup("nobg", { clear = true })
 vim.api.nvim_create_autocmd({ "ColorScheme" }, {
@@ -30,5 +34,16 @@ vim.api.nvim_create_autocmd("LspAttach", {
         end
       end,
     })
+  end,
+})
+
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("wrap_spell"),
+  pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = false
+    -- vim.opt_local.spelllang = { "en", "pt_BR" }
   end,
 })
