46e9562b159e4730565b138bf15da472adb1d3d7
Fernando de Oliveira <5161098+fedeoliv@users.noreply.github.com>
2025-06-06 18:10:48 -0400

    chore: image preview disabled

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -8,7 +8,7 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
 
-    image = { enabled = true },
+    -- image = { enabled = true },
     -- bigfile = { enabled = true },
     -- dashboard = { enabled = true },
     -- explorer = { enabled = true },
