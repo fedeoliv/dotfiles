dcb8eb99a0e3719addddbab81bd0b826472f1fbf
Fernando de Oliveira <5161098+fedeoliv@users.noreply.github.com>
2025-07-03 15:03:39 -0400

    feat: avante replaced by copilot and copilot chat plugins

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" },
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+  },
+}
