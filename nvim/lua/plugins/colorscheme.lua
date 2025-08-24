5f7c08f02e6b1f93410580e27ac1b4492c1ec831
Fernando de Oliveira <5161098+fedeoliv@users.noreply.github.com>
2025-08-24 19:32:27 -0400

    chore: gruvbox colorscheme replaced by vague.nvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,16 @@
 return {
   {
-    "sainnhe/gruvbox-material",
-    lazy = false,
-    priority = 1000,
+    "vague2k/vague.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other plugins
     config = function()
-      vim.g.gruvbox_material_enable_italic = true
-      vim.g.gruvbox_material_transparent_background = 2
+      require("vague").setup({
+        transparent = true,
+      })
     end,
   },
   {
     "LazyVim/LazyVim",
-    opts = { colorscheme = "gruvbox-material" },
+    opts = { colorscheme = "vague" },
   },
 }
