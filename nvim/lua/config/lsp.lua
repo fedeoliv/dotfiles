6bfb2abdec6cc7368e57b731b5881b3f9aba5ce0
Fernando de Oliveira <5161098+fedeoliv@users.noreply.github.com>
2025-11-08 16:17:16 -0500

    chore: unused augroup removed

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -75,12 +75,13 @@ vim.lsp.config('lua_ls', {
 })
 
 vim.api.nvim_create_autocmd('LspAttach', {
-	group = vim.api.nvim_create_augroup('my.lsp', {}),
 	callback = function(args)
 		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+
 		if client:supports_method('textDocument/completion') then
 			-- Optional: trigger autocompletion on EVERY keypress. May be slow!
 			local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+
 			client.server_capabilities.completionProvider.triggerCharacters = chars
 			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
 		end
