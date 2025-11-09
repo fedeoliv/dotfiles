85d10ccef2126386a6b67f767db56a787462a830
Fernando de Oliveira <5161098+fedeoliv@users.noreply.github.com>
2025-11-09 18:03:01 -0500

    feat: gitsigns added

--- nvim/init.lua
+++ nvim/init.lua
@@ -44,6 +44,9 @@ vim.pack.add({
 	-- TODO: integrate colorful-menu with blink or nvim-cmp
 	{ src = "https://github.com/xzbdmw/colorful-menu.nvim" },
 
+	--git
+	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
+
 	-- aesthetics
 	{ src = "https://github.com/sphamba/smear-cursor.nvim" },
 	{ src = "https://github.com/karb94/neoscroll.nvim" },
@@ -156,6 +159,55 @@ require("spider").setup {
 require('mini.pick').setup()
 require('mini.surround').setup()
 require('mini.pairs').setup()
+
+--git
+require('gitsigns').setup({
+	signs = {
+		add = { text = "▎" },
+		change = { text = "▎" },
+		delete = { text = "" },
+		topdelete = { text = "" },
+		changedelete = { text = "▎" },
+		untracked = { text = "▎" },
+	},
+	signcolumn = true,       -- Toggle with `:Gitsigns toggle_signs`
+	numhl = true,            -- Toggle with `:Gitsigns toggle_numhl`
+	linehl = false,          -- Toggle with `:Gitsigns toggle_linehl`
+	word_diff = false,       -- Toggle with `:Gitsigns toggle_word_diff`
+	current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+	current_line_blame_opts = {
+		virt_text = true,
+		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+		delay = 1000,
+		ignore_whitespace = false,
+		virt_text_priority = 100,
+	},
+	watch_gitdir = {
+		follow_files = true,
+	},
+	on_attach = function(buffer)
+		local gs = package.loaded.gitsigns
+
+		local function map(mode, l, r, desc)
+			vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+		end
+
+		-- stylua: ignore start
+		map("n", "<leader>gp", gs.preview_hunk, "Preview hunk")
+		map("n", "]h", gs.next_hunk, "Next Hunk")
+		map("n", "[h", gs.prev_hunk, "Prev Hunk")
+		map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+		map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+		map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
+		map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
+		map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
+		map("n", "<leader>ghp", gs.preview_hunk_inline, "Preview Hunk Inline")
+		map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
+		map("n", "<leader>ghd", gs.diffthis, "Diff This")
+		map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
+		map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+	end,
+})
 
 -- completion
 require("colorful-menu").setup()
